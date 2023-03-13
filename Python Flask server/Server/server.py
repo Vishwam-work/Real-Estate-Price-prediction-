725bd9f60a6b78267a044da6420d22f02df25fc3
Vishwam-work <89005063+Vishwampro@users.noreply.github.com>
2023-03-13 22:53:49 +0530

    Flask server completed

--- Python Flask server/Server/server.py
+++ Python Flask server/Server/server.py
@@ -2,14 +2,27 @@ from flask import Flask,request,jsonify
 import util
 app=Flask(__name__) 
 
-@app.route("/Display")
-def geget_location():
+@app.route("/get_location",methods=['GET'])
+def get_location():
     response = jsonify({
         'location':util.get_location()
     })
+    response.headers.add('Access-control-Allow-Origin','*')
+    return response
 
-
+@app.route("/predict_home_price",methods=['POST'])
+def predict_home_price():
+    total_sqft=float(request.form['total_sqft'])
+    location=(request.form['location'])
+    BHK=int(request.form['BHK'])
+    bath=int(request.form['bath'])
+    response=jsonify({
+        'estimated_price':util.asii_price(location,total_sqft,BHK,bath)
+    })
+    response.headers.add('Access-control-Allow-Origin','*')
+    return response
 
 if __name__=='__main__':
     print("Server in on")
+    util.load_saved_aritfacts()
     app.run() 
--- Python Flask server/Server/util.py
+++ Python Flask server/Server/util.py
@@ -1,25 +1,47 @@
 import json
 import pickle
+import numpy as np
 __location=None
 __data_columns=None
 __model=None
-def get_location():
-    return __location
 
+
+def asii_price(location,sqft,BHK,bath):
+    try:
+        loc_index=__data_columns.index(location.lower())
+    except:
+        loc_index=-1
+    x=np.zeros(len(__data_columns))
+    x[0]=sqft
+    x[1]=BHK
+    x[2]=bath
+    if loc_index>=0:
+        x[loc_index]=1
+    return round(__model.predict([x])[0],2)
+
+# load the artifact means load the pickel and json model
 def load_saved_aritfacts():
     print("LOADING THE ARTIFACTS......")
     global __data_columns
     global __location
     global __model
 
-    with open("./artifacts/columns.json",'r') as f:
-        __data_columns=json.load(f)['data_columns']
-        __location = __data_columns[3:]
-    with open("./artifacts/Home_prices_model.pickle",'rb') as f:
-        __model = pickle.load(f)
+    f = open('columns.json','r')
+    __data_columns=json.load(f)['data_columns']
+    __location = __data_columns[3:]
+    with open("Home_prices_model.pickle",'rb') as f:
+         __model = pickle.load(f)
     print("Loading is Done")
+def get_location():
+    return __location
+def get_data_column():
+    return __data_columns
 
 if __name__=='__main__':
     load_saved_aritfacts()
+
     print(get_location())
-    
+    print(asii_price('1st Block Jayanagar',1000,3,3))
+    print(asii_price('1st Block Jayanagar',1000,2,2))
+    print(asii_price('2nd Stage Nagarbhavi',1000,3,3))
+    print(asii_price('6th Phase JP Nagar',1000,3,3))
